e7fec1f8de9047200eb6fda4cb4b908c90b2775e
justinpatterson <justinpatterson@live.com>
2018-12-21 08:16:14 -0800

    ui updates

--- TicTacToeProject/Assets/Scripts/GamePhases/UIController/END_UIController.cs
+++ TicTacToeProject/Assets/Scripts/GamePhases/UIController/END_UIController.cs
@@ -6,16 +6,29 @@ public class END_UIController : UIController {
     
 	//public Image popUpBody_playerIMG;
 	public Image[] popUp_playerIMGs;
+	public GameObject popUp_container;
 	public GameObject popUp_Winner_container;
 	public GameObject popUp_Draw_container;
+
+	Coroutine _activeCoroutine;
+
+	public override void OpenUI ()
+	{
+		base.OpenUI ();
+		_activeCoroutine = StartCoroutine( DisplayPopupCoroutine() );
+	}
 
 	public override void CloseUI ()
 	{
 		popUp_Draw_container.SetActive(false);
 		popUp_Winner_container.SetActive(false);
+		if(_activeCoroutine != null) 
+		{
+			StopCoroutine( _activeCoroutine );
+			_activeCoroutine = null;
+		}
 		base.CloseUI ();
 	}
-
 
 	public void ReportResults(Results inputResults)
     {
@@ -31,5 +44,21 @@ public class END_UIController : UIController {
 			Debug.Log("container HAS BEEN ENABLED");
 		}
     }
+
+	IEnumerator DisplayPopupCoroutine()
+	{
+		popUp_container.transform.localScale = Vector3.zero;
+		float startTime = Time.unscaledTime;
+		float targetDuration = 0.3f;
+		float percentage = 0f;
+		while( percentage < 1f )
+		{
+			percentage = ( Time.unscaledTime - startTime ) / targetDuration;
+			percentage = Mathf.Clamp( percentage, 0f, 1f);
+			popUp_container.transform.localScale = Vector3.one * percentage;
+			yield return new WaitForEndOfFrame();
+		}
+		yield return new WaitForEndOfFrame();
+	}
 
 }
--- TicTacToeProject/Assets/Scripts/GamePhases/UIController/SHARED_UIController.cs
+++ TicTacToeProject/Assets/Scripts/GamePhases/UIController/SHARED_UIController.cs
@@ -7,13 +7,44 @@ public class SHARED_UIController : UIController {
     public GameObject topBannerContainer;
     public Button backButton;
 
+	public GameObject overlayContainer;
+	public Image overlayBG;
+
+	Coroutine _overlayCoroutine;
+
+	public override void CloseUI ()
+	{
+		base.CloseUI ();
+	}
+
     public void SetSharedUIDisplay (SharedUISettings inputSharedSettings)
     {
 		if(!uiActive) OpenUI();
         topBannerContainer.SetActive(inputSharedSettings.showTopBannerBG);
         backButton.gameObject.SetActive(inputSharedSettings.backButton);
+		overlayContainer.SetActive( inputSharedSettings.overlay );
+		if( inputSharedSettings.overlay )
+		{
+			_overlayCoroutine = StartCoroutine(DispalyOverlayCoroutine());
+		}
     }
-    
+
+	IEnumerator DispalyOverlayCoroutine()
+	{
+		overlayBG.color = new Color(0f,0f,0f,0f);
+		float startTime = Time.unscaledTime;
+		float targetDuration = 0.3f;
+		float percentage = 0f;
+		while( percentage < 1f )
+		{
+			percentage = ( Time.unscaledTime - startTime ) / targetDuration;
+			percentage = Mathf.Clamp( percentage, 0f, 1f);
+			overlayBG.color = new Color(0f,0f,0f,percentage*0.5f);
+			yield return new WaitForEndOfFrame();
+		}
+		yield return new WaitForEndOfFrame();
+	}
+
 }
 
 [System.Serializable]
@@ -21,5 +52,6 @@ public class SharedUISettings
 {
     public bool showTopBannerBG = true;
     public bool backButton;
+	public bool overlay;
 }
 
